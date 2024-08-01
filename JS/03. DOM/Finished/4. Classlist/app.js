c1fa405a1650cddfbd779304239062859921ed2a
Ruhollah JAfari <jruhollah@gmail.com>
2024-08-01 23:43:47 +0330

    BROWSER CONSOLE

--- JS/03. DOM/Finished/4. Classlist/app.js
+++ JS/03. DOM/Finished/4. Classlist/app.js
@@ -8,7 +8,7 @@ const h1 = document.querySelector("h1");
 console.log(h1.classList);
 // console.log((h1.style.color = "skyblue"));
 // console.log((h1.style.fontWeight = "normal"));
-
+ 
 // In EDITOR
 h1.classList.add("styles");
 h1.classList.remove("styles");
