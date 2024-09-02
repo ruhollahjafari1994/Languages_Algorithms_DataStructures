ef41e308511a86849a7da0045581fae90461ecec
Ruhollah JAfari <jruhollah@gmail.com>
2024-09-03 01:01:05 +0330

    Use Object Destructure

--- React/my-app/src/MyCounter.js
+++ React/my-app/src/MyCounter.js
@@ -1,24 +1,24 @@
 import { Component } from "react";
 import Counter from "./components/Counter";
 import './MyCounter.css';
- class MyCounter extends Component{
-    render(){
-        return(
-            <div className="MyCounter">
-                    <h1>شمارنده من</h1>
-                    <br/>
-                    <Counter count={5}/>
-            </div>
-        )
-    }
- }
-// const MyCounter=()=>{
-//     return(
-//         <div className="MyCounter">
-//                      <h1>شمارنده من</h1>
-//                      <br/>
-//                      <Counter/>
-//         </div>
-//     )
-// }
+//  class MyCounter extends Component{
+//     render(){
+//         return(
+//             <div className="MyCounter">
+//                     <h1>شمارنده من</h1>
+//                     <br/>
+//                     <Counter count={50}/>
+//             </div>
+//         )
+//     }
+//  }
+const MyCounter=()=>{
+    return(
+        <div className="MyCounter">
+                     <h1>شمارنده من</h1>
+                     <br/>
+                     <Counter count={520}/>
+        </div>
+    )
+}
 export default MyCounter;
--- React/my-app/src/components/Counter.jsx
+++ React/my-app/src/components/Counter.jsx
@@ -1,21 +1,23 @@
 import { Component } from "react";
-class Counter extends Component{
-render(){
-   // console.log(this.props)
-    return(
-
-        <div>
-        <p>شمارنده: {Math.floor(Math.random()*1000)}</p>
-        </div>
-    )
-}
-}
-// const Counter = ()=>{
+// class Counter extends Component{
+// render(){
+//    // console.log(this.props)
+//    const {count}=this.props;
 //     return(
+
 //         <div>
-//              <p>شمارنده: {Math.floor(Math.random()*1000)}</p>
+//         <p>شمارنده: {count}</p>
 //         </div>
 //     )
 // }
+// }
+//Use Object Destructure
+const Counter = ({count})=>{
+    return(
+        <div>
+             <p>شمارنده: {count}</p>
+        </div>
+    )
+}
 
 export default Counter;
