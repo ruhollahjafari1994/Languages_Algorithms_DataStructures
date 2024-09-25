742f9ff2c7aaf3f61fcc97640b646749af00d038
Ruhollah JAfari <jruhollah@gmail.com>
2024-09-26 00:13:28 +0330

    router

---
+++ React/58_ReactJs_Ghorbani/58_ReactJs_Ghorbani/13.1-React-Router-And-Configuring-Routes/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>کتابخانه من</h1>
+      {/*About - Books*/}      
+    </div>
+  );
+};
+
+export default App;
