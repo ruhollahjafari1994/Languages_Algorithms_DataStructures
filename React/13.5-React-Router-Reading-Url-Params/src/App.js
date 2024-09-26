2bbec0606d9fb526ca470ab55277553d5545e103
Ruhollah JAfari <jruhollah@gmail.com>
2024-09-27 00:15:46 +0330

    Add spinner

--- React/13.5-React-Router-Reading-Url-Params/src/App.js
+++ React/13.5-React-Router-Reading-Url-Params/src/App.js
@@ -1,15 +1,18 @@
-import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+
+import Navbar from "./components/Navbar";
+import Contacts from "./components/contact/Contacts";
 
 import "./App.css";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [getContacts, setContacts] = useState([]);
+
   return (
     <div className="App">
-      <h1>کتابخانه من</h1>
-      <nav style={{ borderBottom: "solid 1px", paddingBottom: "1rem" }}>
-        <Link to="/books">کتاب ها</Link>{"  "}<Link to="/about">درباره ما</Link>
-      </nav>
-      <Outlet />
+      <Navbar />
+      <Contacts contacts={getContacts} loading={loading} />
     </div>
   );
 };
