f9b0db8cd2adcc763e034b47ac3cbec7f6380ebf
Ruhollah JAfari <jruhollah@gmail.com>
2024-09-27 01:30:16 +0330

    Set timer with interval

--- React/13.5-React-Router-Reading-Url-Params/src/App.js
+++ React/13.5-React-Router-Reading-Url-Params/src/App.js
@@ -1,20 +1,53 @@
-import { useState } from "react";
-
-import Navbar from "./components/Navbar";
-import Contacts from "./components/contact/Contacts";
+import { Component } from "react";
 
 import "./App.css";
 
-const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [getContacts, setContacts] = useState([]);
+/*
+ * // NOTE Mounting LifeCycle In Class Components
+ * - constructor()
+ * - static getDerivedStateFromProps(props, state)
+ * - render()
+ * - componentDidMount()
+ */
 
-  return (
-    <div className="App">
-      <Navbar />
-      <Contacts contacts={getContacts} loading={loading} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    console.log("App.js - constructor()");
+
+    this.state = { date: new Date(), showClock: true, color: false };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("App.js - getDerivedStateFromProps()");
+    return state;
+  }
+
+  componentDidMount() {
+    console.log("App.js - componentDidMount()");
+
+    this.timer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    console.log("App.js - componentWillUnmount()");
+
+    clearInterval(this.timer);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    console.log("App.js - render()");
+    return (
+      <div className="App">
+        <p>سلام دوستان عزیز</p>
+        <p>ساعت در حال حاضر برابر : {this.state.date.toLocaleTimeString()}</p>
+      </div>
+    );
+  }
+}
 
 export default App;
