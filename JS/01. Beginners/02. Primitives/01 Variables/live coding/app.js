9df987e4dc19a9fb09b365149d46347e334f2f94
Ruhollah Jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2024-10-17 22:45:14 +0330

    Clean project

--- JS/01. Beginners/02. Primitives/01 Variables/live coding/app.js
+++
@@ -1,7 +0,0 @@
-// (reserved keyword): (variable name): (type of value)
-
-let variable; // undefined
-let name = "HuXn WebDev";
-console.log(name);
-const channelName = "WebDev Academy";
-console.log(channelName);
